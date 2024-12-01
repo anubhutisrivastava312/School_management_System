3e544469e6ee22bd972ef268e9873eaf8f295024
Anubhuti Srivastava <anubhutisrivastava312@gmail.com>
2024-12-01 13:04:20 +0530

    added the procfile

--- main.py
+++ main.py
@@ -19,5 +19,5 @@ app.include_router(
 )
 
 
-if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+# if __name__ == "__main__":
+#     uvicorn.run(app, host="0.0.0.0", port=8000)
